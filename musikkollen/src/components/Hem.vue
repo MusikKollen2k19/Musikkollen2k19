f4cca00162f6434d4bcf5651ad6948960e57394a
tokfrans03 <34898241+tokfrans03@users.noreply.github.com>
2019-11-30 18:17:57 +0100

    Update Hem.vue

--- musikkollen/src/components/Hem.vue
+++ musikkollen/src/components/Hem.vue
@@ -1,10 +1,18 @@
 <template>
     <v-container>
-        <v-btn>Uppdatera</v-btn>
+        <v-btn @click="fetch">Uppdatera</v-btn>
     </v-container>
 </template>
 <script>
+const axios = require("axios");
 export default {
     name: "Hem",
+
+    methods: {
+        async fetch() {
+            const {data} = await axios.get("https://km1wzv5ri1.execute-api.us-east-1.amazonaws.com/v1"); //URLen för databasen, skickas en GET-request till URLen
+            console.log(data);
+        }
+    }
 }
 </script>
